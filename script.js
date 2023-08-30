e93884b5cd2e7f2801830b703259704f1308478e
Massoud Rahmani <Rahmanimassoud8@gmail.com>
2023-08-30 11:28:38 -0700

    added JS functionality

--- script.js
+++ script.js
@@ -34,8 +34,11 @@ let cardItemsData = [{
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     img: "./assets/sneaker1.jpg.jpg",
-}]
+}];
 
+
+// store the data to show which specific item we have selected.
+let basket = [{}];
 
 
 const generateCard = ()=>{
@@ -67,11 +70,48 @@ generateCard();
 
 // increment/decrement and update functions
 const increment = (id)=>{
-    console.log(id);
+    let selectedItem = id;
+    // console.log(selectedItem.id);
+// search to see if the item is already in the basket, if not ad it and if exist increase the count/number of items in the basket.
+    let search = basket.find((element)=> element.id === selectedItem.id)
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    } 
+    else {
+        search.item += 1;
+    }
+    // console.log(basket);
+    update(selectedItem.id);
 }
-const decrement = ()=>{
-    console.log(id);
+const decrement = (id)=>{
+    let selectedItem = id;
+    // console.log(selectedItem.id);
+
+    let search = basket.find((element)=> element.id === selectedItem.id)
+    if(search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    // console.log(basket);
+    update(selectedItem.id);
 }
-const update = ()=>{}
+const update = (id)=>{
+    // search to see if the item is not included than change the number
+    let search = basket.find((element)=> element.id === id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculateNumber();
+}
 
 
+// Add all the numbers and display it in the shopping cart, should increase and decrease.
+
+const calculateNumber = ()=>{
+    let cartNumber = document.getElementById("cartAmount");
+    // console.log(basket);
+    console.log(basket.map((e)=>e.item));
+
+};
